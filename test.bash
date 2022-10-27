360ee92543d173d8a1b1cd38c1e79b9d41ca94f1
Aya <s21c1058fk@s.chibakoudai.jp>
2022-10-27 12:48:34 +0900

    山田消去

---
+++ test.bash
@@ -0,0 +1,18 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2022 Atsuya Sawayama
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
