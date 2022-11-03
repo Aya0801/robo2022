ce305527808941582a52bc5ab407910a9e952aea
Aya <s21c1058fk@s.chibakoudai.jp>
2022-11-03 12:53:13 +0900

    test

--- test.bash
+++ test.bash
@@ -1,25 +1,25 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Atsuya Sawayama
 # SPDX-License-Identifier: BSD-3-Clause
-ng (  ) {
-	echo ${1}行目が違うよ
-	res=1
-	}
+  ng () {
+  echo ${1}行目が違うよ
+  res=1
+  }
 
-	res=0
+    res=0
 
 ### I/O TEST ###
-	out=$(seq 5 | ./plus)
-	[ "${out}" = 15 ] || ng "${LINENO}"
+  out=$(seq 5 | ./plus)
+  [ "${out}" = 15 ] || ng "${LINENO}"
 
 ### STRANGE INPUT ###
-	out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng "${LINENO}"
-[ "${out}" = "" ] || ng "${LINENO}"
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ] || ng "${LINENO}"
+  [ "${out}" = "" ] || ng "${LINENO}"
 
-	out=$(echo | ./plus)
-[ "$?" = 1 ] || ng "${LINENO}"
-[ "${out}" = "" ] || ng "${LINENO}"
+  out=$(echo | ./plus)
+  [ "$?" = 1 ] || ng "${LINENO}"
+  [ "${out}" = "" ] || ng "${LINENO}"
 
-	[ "${res}" = 0 ] && echo OK
-	exit $res
+  [ "${res}" = 0 ] && echo OK
+  exit $res
