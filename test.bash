c2ffcdb2c400f8602072cf21d9276b1f830e6d01
Aya <s21c1058fk@s.chibakoudai.jp>
2022-11-03 13:05:30 +0900

    test

--- test.bash
+++ test.bash
@@ -15,11 +15,11 @@
 
   ### STRANGE INPUT ###
   out=$(echo あ | ./plus)
-  [ "$?" = 1 ] || ng "${LINENO}"
+  [ "$?" = 1 ]      || ng "${LINENO}"
   [ "${out}" = "" ] || ng "${LINENO}"
 
   out=$(echo | ./plus)
-  [ "$?" = 1 ] || ng "${LINENO}"
+  [ "$?" = 1 ]      || ng "${LINENO}"
   [ "${out}" = "" ] || ng "${LINENO}"
 
   [ "${res}" = 0 ] && echo OK
