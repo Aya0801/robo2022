ef2b7453780eac71beb344f33dbc965165666d94
Aya <s21c1058fk@s.chibakoudai.jp>
2022-11-03 13:01:57 +0900

    test

--- test.bash
+++ test.bash
@@ -1,26 +1,27 @@
-#!/bin/bash -xv
-# SPDX-...
-# SPDX-...
+  #!/bin/bash -xv
+  # SPDX-FileCopyrightText: 2022 Atsuya Sawayama
+  # SPDX-License-Identifier: BSD-3-Clause.
 
-ng () {
-	echo ${1}行目が違うよ
-	res=1
-}
+  ng () {
+	  echo ${1}行目が違うよ
+	  res=1
+  }
 
-res=0
+  res=0
 
-### I/O TEST ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "${LINENO}"
+  ### I/O TEST ###
+  out=$(seq 5 | ./plus)
+  [ "${out}" = 15 ] || ng "${LINENO}"
 
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng "${LINENO}"
-[ "${out}" = "" ] || ng "${LINENO}"
+  ### STRANGE INPUT ###
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ] || ng "${LINENO}"
+  [ "${out}" = "" ] || ng "${LINENO}"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ] || ng "${LINENO}"
-[ "${out}" = "" ] || ng "${LINENO}"
+  out=$(echo | ./plus)
+  [ "$?" = 1 ] || ng "${LINENO}"
+  [ "${out}" = "" ] || ng "${LINENO}"
 
-[ "${res}" = 0 ] && echo OK
-exit $res
+  [ "${res}" = 0 ] && echo OK
+  exit $res# SPDX-...
+    
