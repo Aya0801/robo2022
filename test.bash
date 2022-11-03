be937489df397a09e935255860aba886856faa5d
Aya <s21c1058fk@s.chibakoudai.jp>
2022-11-03 12:30:51 +0900

    test

--- test.bash
+++ test.bash
@@ -1,18 +1,25 @@
-#!/bin/bash -xv
+#!/usr/bin/python3
 # SPDX-FileCopyrightText: 2022 Atsuya Sawayama
 # SPDX-License-Identifier: BSD-3-Clause
-
 ng () {
-	echo NG at Line $1
-	res=1
-}
+		echo ${1}行目が違うよ
+			res=1
+		}
 
-res=0
+	res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "${LINENO}"
+### I/O TEST ###
+	out=$(seq 5 | ./plus)
+	[ "${out}" = 15 ] || ng "${LINENO}"
 
+### STRANGE INPUT ###
+	out=$(echo あ | ./plus)
+	[ "$?" = 1 ] || ng "${LINENO}"
+	[ "${out}" = "" ] || ng "${LINENO}"
 
-[ "$res" = 0 ] && echo OK
-exit $res
+	out=$(echo | ./plus)
+	[ "$?" = 1 ] || ng "${LINENO}"
+	[ "${out}" = "" ] || ng "${LINENO}"
 
+	[ "${res}" = 0 ] && echo OK
+	exit $res
