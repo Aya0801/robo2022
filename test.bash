0d303645d194384407ee3b268afb907601760723
Aya0801 <115678848+Aya0801@users.noreply.github.com>
2022-11-29 03:18:06 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -11,11 +11,9 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-<<<<<<< HEAD
 [ "${out}" =  15.0 Odd number ] || ng "${LINENO}"
-=======
-[ "${out}" = 15.0 15.0 Odd numberes ] || ng "${LINENO}"
->>>>>>> e021631b3c9830e24e80e0f62c6c5f0f0db6ac23
+
+
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
