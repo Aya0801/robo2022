1fdff34f0d96510151c606cb6d848ebee88363a7
Atsuya Sawayama <s21c1058fk@s.chibakoudai.jp>
2022-11-29 03:16:06 +0900

    change

--- test.bash
+++ test.bash
@@ -11,7 +11,7 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 15.0 Odd number ] || ng "${LINENO}"
+[ "${out}" =  15.0 Odd number ] || ng "${LINENO}"
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
