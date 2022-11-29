e1a9adbf7bf7eea036289d774ef49d466837332c
Atsuya Sawayama <s21c1058fk@s.chibakoudai.jp>
2022-11-29 12:41:26 +0900

    change

--- test.bash
+++ test.bash
@@ -4,18 +4,15 @@
 
 ng () {
 	echo ${1}行目が違うよ
-        ews=1
+        res=1
 }
 
 res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-<<<<<<< HEAD
 [ "${out}" =  15.0 Odd number ] || ng "${LINENO}"
-=======
-[ "${out}" = 15.0 15.0 Odd numberes ] || ng "${LINENO}"
->>>>>>> e021631b3c9830e24e80e0f62c6c5f0f0db6ac23
+
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
@@ -26,6 +23,6 @@ out=$(echo | ./plus)
 [ "$?" = 1 ] || ng "${LINENO}"
 [ "${out}" = "" ] || ng "${LINENO}"
 
-[ "${res}" = 0 ] && echo OK
+[ "$res" = 0 ] && echo OK
 exit $res
 
